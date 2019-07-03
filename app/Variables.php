1dafc21e795b0bab8eaade6eeec019834bbbb18f
Michał Matysek <hello@michalmatysek.com>
2019-07-03 21:23:16 +0200

    Improve styles

--- app/Variables.php
+++ app/Variables.php
@@ -2,12 +2,17 @@
 
 class App
 {
+    /**
+     * Konfiguracja ustawień aplikacji
+     */
     const APP_NAME = 'Book Library';
     const APP_URL = 'http://booklibrary.staging/';
     const APP_KEY = '8Rz45LQPEnsYfyN7HhXBZdaJ';
     const APP_DEBUG = true;
 
-    // Database
+    /**
+     * Konfiguracja połączenia bazy danych
+     */
     const DB_HOST = 'mariadb';
     const DB_USER = 'book_library_user';
     const DB_NAME = 'book_library';
--- resources/layout/header/head.php
+++ resources/layout/header/head.php
@@ -11,5 +11,5 @@
     <meta name="description"
           content="<?= $metaDescription ?? 'Nowoczesna biblioteka dająca możliwość wypożyczania zbiorów przez internet. Wypożyczk książkę w serwisie, a będzie ona zarezerwowana dla Ciebie przez 3 dni.' ?>">
     <link rel="icon" href="<?= App::APP_URL . '/resources/images/favicon.png' ?>" type="image/x-icon">
-    <link rel="stylesheet" href="/resources/styles/master.css">
+    <link rel="stylesheet" href="/resources/styles/master.min.css">
 </head>
